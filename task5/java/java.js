1e2d49de026982f8ac312c8c07108db080441ce2
nora <nora@gmail.com>
2022-08-22 17:25:47 +0300

    task5

---
+++ task5/java/java.js
@@ -0,0 +1,12 @@
+let num=100000;
+console.log(mum);
+console.log(parseInt("100000NNN"));+
+console.log(Number.isInteger(num));
+console.log((num).toString());
+console.log(Math.round(num));
+console.log(Math.ceil(num));
+console.log(Math.floor(num));
+console.log(Number("100000"));
+console.log(Math.min(100000,400000,600000));
+console.log(Math.max (100000,70,40));
+console.log(Math.pow(10,5));
